3c5370422da967abf80b1cc5c81cacd333d436e0
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-04 21:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 //remove the # in shortHex
+            shortHex = shortHex.replace('#','');
+
+           // in order to repeat the character again 
+            FullHex = shortHex.split('').map((char)=>char+char).join('');
+          
+           //in order to append the removed '#' code
+
+           return '#'+FullHex;
 };
 
 // Do not change the code below.
